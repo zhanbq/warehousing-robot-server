f5ea07e5cd5699fe59c0d5d2399d7ce2e5043ef8
zhanboqi <zhanbq@gmail.com>
2019-03-10 23:46:56 +0800

    基本代码

--- wcs/src/main/java/com/baoshi/wcs/web/controller/admin/AdminController.java
+++ wcs/src/main/java/com/baoshi/wcs/web/controller/admin/AdminController.java
@@ -1,5 +1,6 @@
 package com.baoshi.wcs.web.controller.admin;
 
+import com.baoshi.wcs.common.response.WCSApiResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,6 +11,19 @@ public class AdminController {
     @RequestMapping("/goodsweight")
     public String goodsweight(){
         return "goodsweight";
+    }
+
+    /**
+     * 查询最新的扫码重量数据
+     * @return
+     */
+    @RequestMapping("")
+    public Object getLastGoodsweight(){
+
+        WCSApiResponse<Object> wcsApiResponse = new WCSApiResponse<>();
+
+        return wcsApiResponse;
+
     }
 
 }
