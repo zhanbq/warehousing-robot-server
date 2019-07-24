1e88d8fe37d1dbfa9ca8c907f5afa36bcd2a53f6
zhanboqi <zhanbq@gmail.com>
2019-07-25 00:42:01 +0800

    no message

---
+++ wcs/src/main/resources/static/js/model.js
@@ -0,0 +1,25 @@
+var shelves = {
+    name: '',
+    containerTypes: [],
+    //
+    type: "",
+    code: "",
+    warehouseAreaId: "",
+    warehouseId: "",
+    stackNum: "", //容器堆叠
+    containerType: "",
+    roadwayNo: "",
+    pliesNum: "",
+    columnsNum: "",
+    layerId:"",
+    layers: [{
+        height:""
+    }],
+    columnsId: "",
+    columns: [{
+        wide:"",
+        deep:""
+    }]
+}
+
+var shelvList = [shelves]
